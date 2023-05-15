7cef0e4d7f067e7c4dc498a5b640d1c3b01c369b
John Kluse <j.kluse@gmail.com>
2023-05-15 07:46:01 -0600

    Added study set form

--- public/app.js
+++ public/app.js
@@ -9,26 +9,27 @@ function getQuestions(e) {
 			// console.log(data);
 			for (let el of data) {
 				const html = `
-          <div class="qdiv flip-card-front">
-            <h2 class="question">${el.question}</h2>
-            
-          </div>
-          <div class="qdiv flip-card-back">
-            <h2 class="question">${el.answer}</h2>
-            
-          </div>
+				<div class="card">
+					<div class="qdiv front">
+						<h2 class="question">${el.question}</h2>
+					</div>
+					<div class="qdiv back">
+						<h2 class="question">${el.answer}</h2>
+					</div>
+			</div>
           `;
 				resultDiv.insertAdjacentHTML("beforeend", html);
 			}
+			$(".card").flip({ axis: "x", trigger: "click" });
 		});
 	handleDropDown();
+	// Click Flip
 }
 
 // Drop down
 const library = document.querySelector(".lib-wrapper");
 const dropdownEl = document.querySelector(".dropdown");
 const listItems = document.querySelector(".list-items");
-console.log(listItems);
 
 library.addEventListener("click", handleDropDown);
 
@@ -49,3 +50,44 @@ fetch("/api/subjects")
 			getQuestions(e);
 		});
 	});
+
+// Show form
+const addCardsBtn = document.querySelector(".add-cards");
+const formSection = document.querySelector(".form-section");
+
+addCardsBtn.addEventListener("click", showForm);
+
+function showForm() {
+	resultDiv.innerHTML = "";
+	formSection.innerHTML = "";
+	const html = `
+	<form class="form">
+	<div><label for="topic">Create a new study set: </label></div>
+	<input type="text" name="topic" class="topic" id="topic" placeholder="Enter a new or exsisting topic (ie. Data Structures, Chemistry, etc...)" required pattern=".*\S.*" />
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="question-container">
+		<div class="q-num">1</div>
+		<input type="text" name="question" class="question"  placeholder="Enter term/question...)" required pattern=".*\S.*" />
+		<div class="label-text">TERM</div>
+		<br>
+		<input type="text" name="answer" class="answer"  placeholder="Enter definition/answer...)" required pattern=".*\S.*"/>
+		<div class="label-text">DEFINITION</div>
+		<br>
+
+		<div class="q-num">2</div>
+		<input type="text" name="question" class="question"  placeholder="Enter term/question...)" required pattern=".*\S.*" />
+		<div class="label-text">TERM</div>
+		<br>
+		<input type="text" name="answer" class="answer"  placeholder="Enter definition/answer...)" required pattern=".*\S.*"/>
+		<div class="label-text">DEFINITION</div>
+		<br>
+
+		<input type="submit" value="Submit">
+		</div>
+		</form>
+	`;
+	formSection.insertAdjacentHTML("beforeend", html);
+}
