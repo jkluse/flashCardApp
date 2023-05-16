0e2cd812eb4d64f4c9e966df48fd154c52a2b5dc
John Kluse <j.kluse@gmail.com>
2023-05-16 13:04:16 -0600

    update and delete

--- public/app.js
+++ public/app.js
@@ -1,29 +1,38 @@
 const resultDiv = document.querySelector(".results");
 
 function getQuestions(e) {
+	formSection.innerHTML = "";
+
 	const subj = e.target.textContent;
 	resultDiv.innerHTML = "";
+	editSection.innerHTML = "";
+
 	fetch(`/api/questions/${subj}`)
 		.then((res) => res.json())
 		.then((data) => {
-			// console.log(data);
 			for (let el of data) {
 				const html = `
-				<div class="card">
-					<div class="qdiv front">
-						<h2 class="question">${el.question}</h2>
+				<div class="card" data-id=${el.id}>
+					<div class="qdiv front" data-id=${el.id}>
+						<h2 class="question" data-id=${el.id}>${el.question}</h2>
 					</div>
-					<div class="qdiv back">
+					<div class="qdiv back" data-id=${el.id}>
 						<h2 class="question">${el.answer}</h2>
-					</div>
+						</div>
+			</div>
+			<div class="controls">
+				<img class="right-btn" data-id=${el.id} src="./img/right.svg"></img>
+				<img class="wrong-btn" data-id=${el.id} src="./img/wrong.svg"></img>
+				
 			</div>
           `;
 				resultDiv.insertAdjacentHTML("beforeend", html);
 			}
 			$(".card").flip({ axis: "x", trigger: "click" });
 		});
-	handleDropDown();
-	// Click Flip
+	if (dropdownEl.classList.contains("show")) {
+		handleDropDown();
+	}
 }
 
 // Drop down
@@ -38,56 +47,171 @@ function handleDropDown() {
 }
 
 // on load
-fetch("/api/subjects")
-	.then((res) => res.json())
-	.then((result) => {
-		// console.log(result);
-		for (let sub of result) {
-			const html = `<a class="added-subj" href="#">${sub.subject_name}</a>`;
-			listItems.insertAdjacentHTML("beforeend", html);
-		}
-		listItems.addEventListener("click", (e) => {
-			getQuestions(e);
+function loadLib() {
+	listItems.innerHTML = "";
+	fetch("/api/subjects")
+		.then((res) => res.json())
+		.then((result) => {
+			// console.log(result);
+			for (let sub of result) {
+				const html = `<a class="added-subj" href="#">${sub.subject_name}</a>`;
+				listItems.insertAdjacentHTML("beforeend", html);
+			}
+			listItems.addEventListener("click", (e) => {
+				getQuestions(e);
+			});
 		});
-	});
+}
+loadLib();
 
-// Show form
+// Create new Deck
 const addCardsBtn = document.querySelector(".add-cards");
 const formSection = document.querySelector(".form-section");
 
 addCardsBtn.addEventListener("click", showForm);
 
 function showForm() {
+	if (dropdownEl.classList.contains("show")) {
+		handleDropDown();
+	}
+
 	resultDiv.innerHTML = "";
 	formSection.innerHTML = "";
+	editSection.innerHTML = "";
 	const html = `
 	<form class="form">
-	<div><label for="topic">Create a new study set: </label></div>
-	<input type="text" name="topic" class="topic" id="topic" placeholder="Enter a new or exsisting topic (ie. Data Structures, Chemistry, etc...)" required pattern=".*\S.*" />
-	<br>
-	<br>
-	<br>
-	<br>
-	<div class="question-container">
-		<div class="q-num">1</div>
-		<input type="text" name="question" class="question"  placeholder="Enter term/question...)" required pattern=".*\S.*" />
-		<div class="label-text">TERM</div>
+		<div><label class="form-header" for="topic">Create a new study set: </label></div>
+		<input type="text" name="topic" class="topic" id="topic" placeholder="Enter a topic (ie. Data Structures, Chemistry, etc...)" required />
 		<br>
-		<input type="text" name="answer" class="answer"  placeholder="Enter definition/answer...)" required pattern=".*\S.*"/>
-		<div class="label-text">DEFINITION</div>
 		<br>
+		<br>
+		<div class="question-container">
+			<div class="q-num">1</div>
+			<input type="text" name="question1" class="question"  placeholder="Enter term...)" required  title="This field is required"/>
+			<div class="label-text">TERM</div>
+			<br>
+			<input type="text" name="answer1" class="answer"  placeholder="Enter definition...)" required  title="This field is required"/>
+			<div class="label-text">DEFINITION</div>
+			<br>
 
-		<div class="q-num">2</div>
-		<input type="text" name="question" class="question"  placeholder="Enter term/question...)" required pattern=".*\S.*" />
-		<div class="label-text">TERM</div>
-		<br>
-		<input type="text" name="answer" class="answer"  placeholder="Enter definition/answer...)" required pattern=".*\S.*"/>
-		<div class="label-text">DEFINITION</div>
-		<br>
+			<div class="q-num">2</div>
+			<input type="text" name="question2" class="question"  placeholder="Enter term...)" required  title="This field is required"/>
+			<div class="label-text">TERM</div>
+			<br>
+			<input type="text" name="answer2" class="answer"  placeholder="Enter definition...)" required  title="This field is required"/>
+			<div class="label-text">DEFINITION</div>
+			<br>
 
-		<input type="submit" value="Submit">
-		</div>
+			<input type="submit" value="Submit" class ="form-submit">
+			</div>
 		</form>
 	`;
 	formSection.insertAdjacentHTML("beforeend", html);
+
+	const formEl = document.querySelector(".form");
+	formEl.addEventListener("submit", createDeck);
+}
+
+// Create new Deck
+function createDeck(e) {
+	e.preventDefault();
+	const data = new FormData(e.target);
+
+	fetch("api/questions", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			topic: data.get("topic"),
+			qa_pairs: [
+				[data.get("question1"), data.get("answer1")],
+				[data.get("question2"), data.get("answer2")],
+			],
+		}),
+	})
+		.then((res) => res.json())
+		.then((result) => {
+			loadLib();
+			showForm();
+		});
+}
+
+// Edit Button
+const editBtn = document.querySelector(".edit-btn");
+const editSection = document.querySelector(".edit-cards");
+editBtn.addEventListener("click", editCards);
+
+function editCards() {
+	resultDiv.innerHTML = "";
+	formSection.innerHTML = "";
+	editSection.innerHTML = "";
+
+	fetch("/api/subjects")
+		.then((res) => res.json())
+		.then((result) => {
+			// console.log(result);
+			for (let sub of result) {
+				const html = `
+			<div class="sub-edit-wrapper">
+				<p class="added-subj" >${sub.subject_name}</p>
+				<button class='edit-btn'><img class="rename-btn" data-id=${sub.id} src="./img/edit.svg"></img></button>
+				<button class='delete-btn'><img data-id=${sub.id} src="./img/delete.svg"></button>
+			</div>
+			`;
+				editSection.insertAdjacentHTML("beforeend", html);
+			}
+			const deleteBtns = document.querySelectorAll(".delete-btn");
+			deleteBtns.forEach((btn) => btn.addEventListener("click", deleteTopic));
+
+			const renameBtn = document.querySelectorAll(".rename-btn");
+			renameBtn.forEach((btn) => btn.addEventListener("click", rename));
+		});
+}
+
+function rename(e) {
+	const p = this.closest("div").firstElementChild;
+	const originalText = p.textContent;
+	const form = `
+	<form class="edit-form">
+	<input id="text" name="text" value=${originalText} required/>
+	<button type="submit">Update</button>
+	</form>
+	`;
+
+	p.innerHTML = form;
+	const editText = document.querySelector("#text");
+	const editForm = document.querySelector(".edit-form");
+	editForm.addEventListener("submit", (e) => {
+		handleEditName(e, originalText);
+	});
+	editText.focus();
+}
+
+function handleEditName(e, originalText) {
+	e.preventDefault();
+	const editForm = document.querySelector(".edit-form");
+	const data = new FormData(e.target);
+	const updatedName = data.get("text");
+	console.log(originalText, updatedName);
+
+	// replace input form with updated name
+	// 1. fetch PATCH with updatedName
+	fetch(`/api/subjects/${originalText}/${updatedName}`, {
+		method: "PATCH",
+	});
+	// 2. Reload editCards
+	editCards();
+	loadLib();
+}
+
+function deleteTopic(e) {
+	const topicId = e.target.getAttribute("data-id");
+	fetch(`/api/subjects/${topicId}`, { method: "DELETE" })
+		.then((res) => res.json())
+		.then((result) => {
+			console.log(result);
+			editCards();
+			loadLib();
+		});
 }
--- server.js
+++ server.js
@@ -8,12 +8,13 @@ const db = new pg.Pool({
 const server = express();
 const PORT = process.env.PORT || 3000;
 
+server.use(express.json());
 server.use(express.static("public"));
 
 server.get("/api/questions/:sub", (req, res) => {
 	const sub = req.params.sub;
 
-	db.query("SELECT s.subject_name, qa.question, qa.answer FROM question_answer qa JOIN subject s ON s.id = qa.subject_id WHERE s.subject_name = $1;", [sub]).then((result) => {
+	db.query("SELECT qa.id, s.subject_name, qa.question, qa.answer FROM question_answer qa JOIN subject s ON s.id = qa.subject_id WHERE s.subject_name = $1;", [sub]).then((result) => {
 		res.json(result.rows);
 	});
 });
@@ -22,6 +23,70 @@ server.get("/api/subjects", (req, res) => {
 	db.query("SELECT * FROM subject").then((result) => {
 		res.send(result.rows);
 	});
+});
+
+server.patch("/api/subjects/:oldName/:newName", (req, res) => {
+	const old = req.params.oldName;
+	const updated = req.params.newName;
+	if (!req.params.oldName || !req.params.newName) {
+		res.status(400).json({ status: "fail", message: "Usage: /api/subjects/(original name)/(updated name)" });
+	}
+
+	db.query("UPDATE subject SET subject_name = $1 WHERE subject_name = $2 RETURNING *;", [updated, old]).then((result) => {
+		// console.log(result.rows);
+		if (result.rows.length === 0) return res.status(500).json({ status: "fail", message: "Sever error! Try again" });
+
+		res.status(201).json({ status: "success", data: result.rows[0] });
+	});
+});
+
+server.post("/api/questions", (req, res) => {
+	const topic = req.body.topic;
+	const qa_pairs = req.body.qa_pairs;
+
+	//create new topic RETURNING id
+	db.query("INSERT INTO subject (subject_name) VALUES ($1) RETURNING id", [topic])
+		.then((result) => {
+			if (result.rows.length === 0) res.sendStatus(500);
+
+			//With id, add all qa_pairs to question_answer table
+			const id = Number(result.rows[0].id);
+
+			db.query("INSERT INTO question_answer (question, answer, subject_id) VALUES($1, $2, $3), ($4, $5, $6) RETURNING *", [
+				qa_pairs[0][0],
+				qa_pairs[0][1],
+				id,
+				qa_pairs[1][0],
+				qa_pairs[1][1],
+				id,
+			]).then((result) => {
+				if (result.rows.length === 0) res.sendStatus(500);
+			});
+			res.status(201).json({
+				message: "success",
+				data: result.rows,
+			});
+		})
+		.catch((err) => {
+			throw err;
+		});
+});
+
+server.delete("/api/subjects/:id", (req, res) => {
+	const id = Number(req.params.id);
+	if (!id || !Number.isInteger(id)) return res.status(400).json({ status: "fail", reason: "Missing or invalid id" });
+
+	db.query("DELETE FROM question_answer WHERE subject_id = $1 returning subject_id;", [id])
+		.then((result) => {
+			if (result.rows.length === 0) return res.status(404).json({ status: "fail", reason: "Topic Not found" });
+
+			return db.query("DELETE FROM subject WHERE id = $1 RETURNING subject_name;", [id]);
+		})
+		.then((result) => {
+			if (result.rows.length === 0) return res.status(404).json({ status: "fail", reason: "Topic Not found" });
+
+			res.status(200).json({ status: "success", deleted: result.rows[0].subject_name });
+		});
 });
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
